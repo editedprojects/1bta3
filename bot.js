674aac5da16405ce8a516c30c9918f485885c845
EdiTeD's Projects <42066039+editedprojects@users.noreply.github.com>
2018-11-04 08:21:33 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('I am ready!');
+     client.user.setGame("RoyaleGames","http://twitch.tv/HP");
+});
+
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', '•-chat')
+    let memberavatar = member.user.avatarURL
+      if (!channel) return;
+message.channel.send('**~~~Welcome~ To RG. :beers: ~~**')
+}
+    });
+    
+     client.login(process.env.BOT_TOKEN); 
